83f52f6ab670fcda0abfd465c5906d63f3039110
Vladimir Rudomanenko <rudomanenkovladimir@gmail.com>
2023-06-12 19:26:30 +0300

    refactor salesbox config

--- config/salesbox.php
+++ config/salesbox.php
@@ -3,5 +3,5 @@
 return [
     'company_id' => env('SALESBOX_COMPANY_ID'),
     'api_base_url' => env('SALESBOX_API_BASE_URL', 'https://prod.salesbox.me/api'),
-    'phone' => '+380983269792'
+    'phone' => env('SALESBOX_PHONE')
 ];
