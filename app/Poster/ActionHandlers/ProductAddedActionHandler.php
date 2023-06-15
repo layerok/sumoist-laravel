0f56b50a16d3993d4de78aaafaa2b764c69d35af
Vladimir Rudomanenko <rudomanenkovladimir@gmail.com>
2023-06-15 14:15:50 +0300

    create product in salesbox on poster hook

--- app/Poster/ActionHandlers/ProductAddedActionHandler.php
+++ app/Poster/ActionHandlers/ProductAddedActionHandler.php
@@ -2,11 +2,14 @@
 
 namespace App\Poster\ActionHandlers;
 
+use App\Poster\Entities\Category;
+use App\Poster\Entities\Product;
 use App\Poster\PosterApiException;
 use App\Salesbox\Facades\SalesboxApi;
 use poster\src\PosterApi;
 
-class ProductAddedActionHandler extends AbstractActionHandler {
+class ProductAddedActionHandler extends AbstractActionHandler
+{
 
     public function handle(): bool
     {
@@ -18,17 +21,73 @@ class ProductAddedActionHandler extends AbstractActionHandler {
 
         $salesboxProduct = $collection->firstWhere('externalId', $this->getObjectId());
 
-        if($salesboxProduct) {
+        if ($salesboxProduct) {
             return false;
         }
 
-        $posterProducts = PosterApi::menu()->getProduct([
+        $posterProduct = PosterApi::menu()->getProduct([
             'product_id' => $this->getObjectId()
         ]);
 
-        if(!isset($posterProducts->response) || !$posterProducts->response) {
-            throw new PosterApiException('getProduct', $posterProducts);
+        if (!isset($posterProduct->response) || !$posterProduct->response) {
+            throw new PosterApiException('getProduct', $posterProduct);
         }
+
+        $posterProductEntity = new Product($posterProduct->response);
+        $spot = $posterProductEntity->getSpots()[0];
+
+        $offer = [
+            'units' => 'pc',
+            'stockType' => 'endless',
+            'available' => false, // todo: don't forget to show product by default
+            'price' =>  intval($posterProductEntity->getPrice($spot->spot_id)) / 100,
+
+            'externalId' => $posterProductEntity->getProductId(),
+        ];
+
+
+        $offer['names'] = [
+            [
+                'name' => $posterProductEntity->getProductName(),
+                'lang' => 'uk' // todo: move this value to config, or fetch it from salesbox api
+            ]
+        ];
+
+        $offer['descriptions'] = [];
+        $offer['categories'] = [];
+
+        if(!!$posterProductEntity->getMenuCategoryId()) {
+            $posterCategory = PosterApi::menu()->getCategory([
+                'category_id' => $posterProductEntity->getMenuCategoryId()
+            ]);
+            if (!isset($posterCategory->response) || !$posterCategory->response) {
+                throw new PosterApiException('getCategory', $posterCategory);
+            }
+            $posterCategoryEntity = new Category($posterCategory->response);
+
+            $salesboxCategory = SalesboxApi::getCategoryByExternalId($posterCategoryEntity->getId());
+
+            if($salesboxCategory) {
+                $offer['categories'][] = $salesboxCategory['id'];
+            } else {
+                // create category in salesbox
+            }
+        }
+
+        $offer['photos'] = [];
+        if ($posterProductEntity->getPhoto()) {
+            $offer['photos'][] = [
+                'url' => config('poster.url') . $posterProductEntity->getPhotoOrigin(),
+                'previewURL' => config('poster.url') . $posterProductEntity->getPhoto(),
+                'order' => 0,
+                'type' => 'image',
+                'resourceType' => 'image'
+            ];
+        }
+
+        SalesboxApi::createManyOffers([
+            'offers' => [$offer]
+        ]);
 
         return true;
     }
